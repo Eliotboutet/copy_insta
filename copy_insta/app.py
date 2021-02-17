ef57c1a08a45ba2c8a85cf87418f6cfb37677a48
Eliotboutet <bouteteliot@gmail.com>
2021-02-17 10:06:15 +0100

    DB setup

--- copy_insta/app.py
+++ copy_insta/app.py
@@ -1,11 +1,27 @@
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from database import models
+from database.database import db, init_database
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Insta.sqlite3'
+db.init_app(app)
+
+
+with app.app_context():
+    init_database()
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    engineers = get_all_engineers()
+    return engineers.email
+
+
+def get_all_engineers():
+    return models.Engineer.query.filter_by(username='ezraa').first()
 
 
 if __name__ == '__main__':
