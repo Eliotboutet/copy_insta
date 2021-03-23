37a33358532f71f3bc5d8c19b8aedc9fabee789d
Lucas Yakhontoff <lucas.yakhontoff@imt-atlantique.net>
2021-03-23 18:40:45 +0100

    proper

--- copy_insta/database/main.py
+++ copy_insta/database/main.py
@@ -6,11 +6,11 @@ main = Blueprint('main', __name__)
 
 
 @main.route('/')
-def index():
+def index():    
     return flask.render_template('home_page.html.jinja2')
 
 
 @main.route('/profile')
 @login_required
 def profile():
-    return flask.render_template('profile_temporary.html.jinja2', name=current_user.name)
+    return flask.render_template('profile_page.jinja2', name=current_user.name)
