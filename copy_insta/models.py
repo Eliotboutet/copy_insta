46925fcb54516847c99cc8b6720cb9322f49629c
Lucas Yakhontoff <lucas.yakhontoff@imt-atlantique.net>
2021-03-26 11:37:24 +0100

    encore re-ajouté la db

--- copy_insta/models.py
+++ copy_insta/models.py
@@ -7,3 +7,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Pictures(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    who_posted = db.Column(db.Interval, nullable=False)
+    likes = db.Column(db.Integer)
+    comments = db.Column(db.TEXT)
