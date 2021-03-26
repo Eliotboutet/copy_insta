7ab47338e3d9687126d7c7a7b3c66ed44da0c3ec
Lucas Yakhontoff <lucas.yakhontoff@imt-atlantique.net>
2021-03-26 11:53:55 +0100

    models

--- copy_insta/models.py
+++ copy_insta/models.py
@@ -3,12 +3,14 @@ from app import db
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
 class Pictures(UserMixin, db.Model):
+    __tablename__='pictures'
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     who_posted = db.Column(db.Interval, nullable=False)
     likes = db.Column(db.Integer)
